4b110f598e55ea4f38d1164c8a416694e317acbd
mustafashokry555 <mustafashokry555@gmail.com>
2020-05-05 04:46:46 +0200

    Add Update Delete funcs

--- js/index.js
+++ js/index.js
@@ -25,8 +25,8 @@ addBtn.addEventListener("click",function(){
     }
     products.push(product);
     localStorage.setItem("products", JSON.stringify(products));
-    resetInputs();
     showProduct();
+    resetInputs();
 })
 
 
@@ -37,13 +37,15 @@ function showProduct()
     var proContainer = "";
     for( var i = 0 ; i<products.length ; i++)
     {
+        var j=i+1;
         proContainer += `<tr>
+        <td>` + j + `</td>
         <td>` + products[i].name + `</td>
         <td>` + products[i].price + `</td>
         <td>` + products[i].category + `</td>
         <td >` + products[i].desc + `</td>
-        <td> <i onclick = "(` + i + `)" class="icon-style  text-danger  far fa-trash-alt"></i> </td>
-        <td> <i onclick = "(` + i + `)" class="icon-style text-success fas fa-pen"></i> </td>
+        <td> <i onclick = "showDeleteAlert(` + i + `)" class="icon-style  text-danger  far fa-trash-alt"></i> </td>
+        <td> <i onclick = "update(` + i + `)" class="icon-style text-success fas fa-pen"></i> </td>
         </tr>`
     }
     document.getElementById("tBody").innerHTML = proContainer ;
@@ -96,11 +98,72 @@ function showSearchReasult(index)
 
 
 //delete funcs
-function showDeleteAlert()
+function showDeleteAlert(index)
 {
+    document.getElementById("deleteProName").innerHTML = products[index].name ;
+    document.getElementById("deleteBtns").innerHTML = 
+    `<button  onclick = "yesDelete(` + index + `)" class="btn py-2 px-4 mr-3 badge-danger">Yes</button>
+    <button id="noD" onclick = "noDelete()" class="btn py-2 px-4 btn-light">No</button>`
+    document.getElementById("deleteAlert").style.display = "flex";
+}
 
+function yesDelete(index)
+{
+    document.getElementById("deleteAlert").style.display = "none";
+    products.splice(index,1);
+    localStorage.setItem("products", JSON.stringify(products));
+    showProduct();
+}
+
+function noDelete()
+{
+    document.getElementById("deleteAlert").style.display = "none";
+}
+
+//update funcs
+var updateConfirmIndex = null;
+function update(index)
+{
+    addBtn.style.display ="none" ;
+    document.getElementById("updateBtn").style.display="inline-block";
+    document.getElementById("cancelBtn").style.display="inline-block";
+    productName.value = products[index].name;
+    productPrice.value = products[index].price;
+    productCategory.value = products[index].category;
+    productDesc.value = products[index].desc;
+
+    updateConfirmIndex = index ;
 }
 
 
+//update btn fun 
+document.getElementById("updateBtn").addEventListener("click" ,function()
+    {
+        updateConfirm(updateConfirmIndex);
+    });
+function updateConfirm(index)
+{
+
+    products[index].name = productName.value,
+    products[index].price = productPrice.value,
+    products[index].category = productCategory.value,
+    products[index].desc = productDesc.value,
+
+    localStorage.setItem("products", JSON.stringify(products));
+    
+    addBtn.style.display ="inline-block" ;
+    document.getElementById("updateBtn").style.display="none";
+    document.getElementById("cancelBtn").style.display="none";
+    showProduct();
+    resetInputs();
+}
 
 
+//cancel update btn fun
+document.getElementById("cancelBtn").addEventListener("click" ,function()
+{
+    addBtn.style.display ="inline-block" ;
+    document.getElementById("updateBtn").style.display="none";
+    document.getElementById("cancelBtn").style.display="none";
+    resetInputs();
+});
